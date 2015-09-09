a3c168ff8eccb8bde8a855dba2a3a66d5d7990d8
mikechristopher <mikechristopher@gmail.com>
2015-09-09 12:39:51 +0100

    Added files

---
+++ include/db_config.php
@@ -0,0 +1,27 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin1');
+define('DB_DATABASE', 'project2');
+
+define("BASE_URL","/Project2");
+
+	
+
+
+/*class DB_con {
+	public $mysqli1;
+    function __construct() {
+       $this->mysqli1 = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+		
+		if(mysqli_connect_errno()) {
+ 
+			echo "Error: Could not connect to database.";
+ 
+		exit;
+ 
+		}
+    }
+   
+}*/
+?>
